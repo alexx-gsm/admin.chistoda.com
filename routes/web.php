13a2bf39d67e6504ef5c8d57923230281d063192
alexx-gsm <alexx-gsm@yandex.ru>
2017-04-01 22:24:59 +0700

    add registration (temporary)

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,24 @@
 <?php
 
-//Route::get('/admin', 'Admin\IndexController@index');
-
 Route::get('/', function () {
     return redirect()->to('/admin/login');
 });
-//Route::get('/register', function () {
-//    return Redirect::to('/admin/login');
-//});
-//
-//Route::get('/password/reset', function () {
-//    return Redirect::to('/admin/login');
-//});
-//
-//
-///* API */
-//Route::group(['prefix' => 'api'], function () {
-//    Route::get('price', 'Api\PriceController@index');
-//});
-//
-//
+
+Route::get('/register', function () {
+    return Redirect::to('/admin/login');
+});
+
+Route::get('/password/reset', function () {
+    return Redirect::to('/admin/login');
+});
+
+
+/* API */
+Route::group(['prefix' => 'api'], function () {
+    Route::get('price', 'Api\PriceController@index');
+});
+
+
 /*  ADMIN ROUTES */
 Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
     Route::get('/', 'Admin\IndexController@index');
@@ -30,5 +29,3 @@ Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
 Route::group(['prefix' => 'admin'], function () {
     Auth::routes();
 });
-
-//Auth::routes();
